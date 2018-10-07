d34ce3639d8f919db58428c736bc67b98e9abeac
Mustafa Selcuk Caglar <selcukcaglar08@gmail.com>
2018-10-07 16:10:39 +0300

    make the codes look smoother.

--- include/pipe/algorithm/pipe.h
+++ include/pipe/algorithm/pipe.h
@@ -2,12 +2,14 @@
 #include <utility>
 #include <pipe/traits.h>
 
-namespace pipe { namespace algorithm { namespace details {
-
-template <typename Generator, typename Algorithm, typename = std::enable_if<pipe::is_generator<Generator>::value>::type>
-auto operator|(Generator& gen, Algorithm&& algorithm)
-{
-    return algorithm(std::move(gen));
-}
-
-}}} // namespace pipe::algorithm::details
+namespace pipe { 
+	namespace algorithm { 
+		namespace details {
+			template <typename Generator, typename Algorithm, typename = std::enable_if<pipe::is_generator<Generator>::value>::type>
+			auto operator|(Generator& gen, Algorithm&& algorithm)
+			{
+				return algorithm(std::move(gen));
+			}
+		}
+	}
+} // namespace pipe::algorithm::details
